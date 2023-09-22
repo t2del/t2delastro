1dfa33d18b86de6629c9130c2d6b3a59f8aa598b
Dennis Macapagal <macapagal.dennis@gmail.com>
2023-09-23 03:06:54 +0800

    Update nav.js

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,15 +1,15 @@
 export const nav = [
 	{
 		title: 'Home',
-		slug: '/',
+		slug: '/t2delastro/',
 	},
 	{
 		title: 'About',
-		slug: '/about',
+		slug: '/t2delastro/about',
 	},
 	{
 		title: 'Portfolio',
-		slug: '/portfolio',
+		slug: '/t2delastro/portfolio',
 	},
 
 ];
