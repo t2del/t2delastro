361fb2747c7b74c4ce25a6f3f46ca764cb2a4a34
t2del <macapagal.dennis@gmail.com>
2023-09-25 22:36:25 +0800

    minor changes on blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		imgalt: z.string().optional(),
 	}),
 });
 
