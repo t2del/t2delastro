bdd1b346175f7956d05a236daf4fdd89e8803e3a
t2del <macapagal.dennis@gmail.com>
2023-10-05 00:24:38 +0800

    added submenu in header nav

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,17 +1,33 @@
 export const nav = [
 	{
-		title: 'Home',
-		slug: '/t2delastro/',
+	  title: 'Home',
+	  slug: '/t2delastro/',
 	},
 	{
-		title: 'About',
-		//slug: '#about',
-		slug: '/t2delastro/about',
+	  title: 'About',
+	  slug: '/t2delastro/about',
 	},
 	{
-		title: 'Portfolio',
-		//slug: '#portfolio',
-		slug: '/t2delastro/portfolio',
+	  title: 'Blog',
+	  slug: '/t2delastro/blog',
+	  submenu: [
+		{
+		  title: 'First post',
+		  slug: '/t2delastro/blog/first-post',
+		},
+		{
+			title: 'Second post',
+			slug: '/t2delastro/blog/second-post',
+		},
+		{
+			title: 'Third post',
+			slug: '/t2delastro/blog/third-post',
+		},
+	  ],
 	},
-
-];
+	{
+	  title: 'Portfolio',
+	  slug: '/t2delastro/portfolio',
+	},
+  ];
+  
