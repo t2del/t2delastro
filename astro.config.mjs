dac257f59b1678c2dad52fc095d74aa601eadee3
t2del <macapagal.dennis@gmail.com>
2023-10-16 22:30:02 +0800

    canonincal url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://t2delastro.github.io/t2delastro/',
+    site: 'https://t2del.github.io/t2delastro/',
     base: '/t2delastro/',
 });
