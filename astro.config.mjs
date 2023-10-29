6c8e87b3c10f88ae60bc7038df09174ea94fac27
t2del <macapagal.dennis@gmail.com>
2023-10-29 20:16:14 +0800

    added ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-//import node from "@astrojs/node";
+import nodejs from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://t2del.github.io/t2delastro/',
     base: '/t2delastro/',
-    // output: 'server',
-    // adapter: node({
-    //     mode: 'standalone'
-    // }),
+    output: 'server',
+    adapter: nodejs(),
 });
